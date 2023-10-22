ea2c7366698201ac23e5f12dbcc96990e6c695a9
Paul Schur <paul.schur@gmx.de>
2023-10-22 21:59:34 +0000

    add school controller

---
+++ app/Http/Controllers/Api/SchoolController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\School;
+use Illuminate\Http\Request;
+
+class SchoolController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(School $school)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, School $school)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(School $school)
+    {
+        //
+    }
+}
