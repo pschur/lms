a457d0e974050e02938519a824271c9236da9898
Paul Schur <paul.schur@gmx.de>
2023-10-22 22:00:13 +0000

    add school_class table/model/policy

---
+++ app/Http/Controllers/SchoolClassController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SchoolClass;
+use Illuminate\Http\Request;
+
+class SchoolClassController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SchoolClass $schoolClass)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SchoolClass $schoolClass)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SchoolClass $schoolClass)
+    {
+        //
+    }
+}
